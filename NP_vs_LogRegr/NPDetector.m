431b00b40f9aed67ef08c4f8511eb912ef6a5a59
DavinSeq <158449279+DavinSeq@users.noreply.github.com>
2026-02-03 11:07:46 +0530

    Update NPDetector.m

--- NP_vs_LogRegr/NPDetector.m
+++ NP_vs_LogRegr/NPDetector.m
@@ -1,8 +1,8 @@
 clc; clear; close all;
 
-A        = 1.0;       % Signal amplitude
+A        = 1.0;       % Signal amplitude    (Push Signal Amplitude towards 0 to see the performance decrease)
 sigma    = 0.5;       % Noise standard deviation
-P_FA     = 0.05;      % Target false alarm probability
+P_FA     = 0.05;      % Target false alarm probability    (Increase this to see performance increase)
 trials   = 1e5;       % Monte Carlo trials
 
 
@@ -22,14 +22,14 @@ for k = 1:trials
     
     if H == 0
         x = sigma * randn;      % H0
-        true_label = 0;
+        true_label = 0;    % true_label = 0 denotes noise only
     else
         x = A + sigma * randn;  % H1
-        true_label = 1;
+        true_label = 1;    % true_label = 1 denotes signal + noise
     end
     
     % NP Detector
-    decision = (x > eta);
+    decision = (x > eta);    % If x>eta decision = 1 (signal+noise), else decision = 0 (noise only)
     
     % Count error
     if decision ~= true_label
